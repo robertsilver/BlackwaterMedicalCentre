d6afe6414eeec6ed1cc28b4d922a1c53ca987e42
RobertSilver <robert.silver@outlook.com>
2014-04-17 03:50:16 +0100

    updates

---
+++ BlackwaterMedicalCentre/App/app.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var app = angular.module('blackwaterMedicalCentreApp', ['ngRoute']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'homeController',
+                templateUrl: 'app/views/home.html'
+            })
+            .when('/orders/:customerId', {
+                controller: 'OrdersController',
+                templateUrl: 'app/views/orders.html'
+            })
+            .otherwise({ redirectTo: '/' });
+    });
+
+}());
